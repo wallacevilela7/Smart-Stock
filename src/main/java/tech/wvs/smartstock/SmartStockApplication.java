e6f8be6b6c4ed3a3c43a3d4e5111438abb69ef77
wallacevilela7 <wallace.vilela9@gmail.com>
2025-02-18 19:28:57 -0300

    Create App

---
+++ src/main/java/tech/wvs/smartstock/SmartStockApplication.java
@@ -0,0 +1,13 @@
+package tech.wvs.smartstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartStockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartStockApplication.class, args);
+    }
+
+}
